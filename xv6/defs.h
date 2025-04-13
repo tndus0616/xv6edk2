6b84f4f6a814f5d3dab428d5e3dce28d8a9a8c75
tndus0616 <147567647+tndus0616@users.noreply.github.com>
2025-04-13 16:53:55 +0900

    Update defs.h

--- xv6/defs.h
+++ xv6/defs.h
@@ -155,6 +155,7 @@ int             argstr(int, char**);
 int             fetchint(uint, int*);
 int             fetchstr(uint, char**);
 void            syscall(void);
+int             sys_uthread_init(void);
 
 // timer.c
 void            timerinit(void);
