910ceb6ecba59f50dc8b40d209d8de9183aaacc3
tndus0616 <147567647+tndus0616@users.noreply.github.com>
2025-04-14 20:53:29 +0900

    Add files via upload

--- xv6/proc.h
+++ xv6/proc.h
@@ -48,9 +48,12 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
-  uint scheduler;              // user-level scheduler function pointer
+  char name[16];
+  uint scheduler;              // address of the user-level scheduler
 };
+
+struct proc *myproc(void);
+int uthread_init(int address);
 
 // Process memory is laid out contiguously, low addresses first:
 //   text
--- xv6/user.h
+++ xv6/user.h
@@ -1,3 +1,4 @@
+#include "types.h"
 struct stat;
 struct rtcdate;
 
@@ -23,7 +24,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int uthread_init(int);
+int uthread_init(int address);
 
 // ulib.c
 int stat(char*, struct stat*);
